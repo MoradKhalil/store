c444f7d1afeda79124c2e7a8037ec569b2262d5e
MoradKhalil <mourad,kha92@gmail.com>
2023-11-18 02:47:19 +0200

    first commit

---
+++ src/main/java/com/example/store/store/StoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication - annotation is an entry point that runs spring application
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
